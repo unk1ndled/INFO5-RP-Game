2672404d099c77e57cf42740acd436bea6842715
unk1ndled <amine.games2002@gmail.com>
2025-12-10 22:20:13 +0100

    initial

---
+++ src/main/java/com/polytech/App.java
@@ -0,0 +1,9 @@
+package com.polytech;
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
+}
